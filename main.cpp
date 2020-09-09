edcf5a0f562549842fe95414af4fbc645e38477e
Stephen Anderson <sanderson86@mail.csuchico.edu>
2020-09-09 11:58:05 -0700

    Made modification

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include<iostream>
-#include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
   char input;
+  vector <string> list;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+  do
+  {
+    cout<<"\n==GROCERY LIST MANAGER==";
+    cout<<"\nEnter your choice: ";
+    cout<<"\n (A)dd an item";
+    cout<<"\n (Q)uit";
+    cout<<"\nYour choice (A/Q): ";
+    cin>>input;
 
+    if( input == 'a' || input == 'A' )
+    {
+        string item;
+        cout<<"What item? ";
+        cin.ignore();
+        getline(cin,item);
+        list.push_back(item);
+    }
+  }while( input != 'q' && input != 'Q' );
+  for(int i = 0; i < list.size(); i++){
+    cout << i <<" " + list[i] << endl;
+  }
   return 0;
 }

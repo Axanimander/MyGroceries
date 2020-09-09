0e30114f38cf02f67fae04afaa204c1ee9c0f277
Stephen Anderson <sanderson86@mail.csuchico.edu>
2020-09-09 15:20:02 -0700

    Fixed no-item message

--- main.cpp
+++ main.cpp
@@ -25,8 +25,12 @@ int main()
         list.push_back(item);
     }
   }while( input != 'q' && input != 'Q' );
+  if(list.size() > 0){
   for(int i = 0; i < list.size(); i++){
     cout << i <<" " + list[i] << endl;
+  }}
+  else{
+    cout << "there are no items on the list\n";
   }
   return 0;
 }
